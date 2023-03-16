d576528e098abc79d6b4b81eb6d6225b9302badc
Jean-Yanis <jean-yanis.jeffroy@epitech.eu>
2023-03-16 11:39:45 +0100

    feat(main) add resizable to drawing

--- src/shape/draw/circle.c
+++ src/shape/draw/circle.c
@@ -10,6 +10,8 @@
 static void draw_line_circle(game_t *game, int i, int j, float radius)
 {
     sfVector2f pos = game->board->mouse_pos;
+    pos.x /= game->board->scale.x;
+    pos.y /= game->board->scale.y;
     if (VECT2F_MANHATTAN(pos, game->board->last_pos) > 1) {
         sfVector2f dir = VECT2F_SUB(pos, game->board->last_pos);
         sfVector2f dir_norm = VECT2F_NORMALIZE(dir);
@@ -18,9 +20,9 @@ static void draw_line_circle(game_t *game, int i, int j, float radius)
         line = VECT2F_ADD(line, dir_norm);
         (i * i + j * j <= radius * radius
         && i * i + j * j >= (radius - BRUSH_SIZE) * (radius - BRUSH_SIZE)
-        && line.x + i < sfSprite_getTextureRect(game->board->sprite).width
+        && line.x + i < sfSprite_getGlobalBounds(game->board->sprite).width
         && line.x - i >= 0 && line.y - j >= 0
-        && line.y + j < sfSprite_getTextureRect(game->board->sprite).height) ?
+        && line.y + j < sfSprite_getGlobalBounds(game->board->sprite).height) ?
         ({  sfImage_setPixel(IMAGE, line.x + i, line.y + j, BRUSH_COLOR);
             sfImage_setPixel(IMAGE, line.x - i, line.y + j, BRUSH_COLOR);
             sfImage_setPixel(IMAGE, line.x + i, line.y - j, BRUSH_COLOR);
@@ -35,14 +37,16 @@ void draw_circle_on_sfimage(game_t *game)
     float radius = BRUSH_SIZE;
     sfColor color = game->board->color;
     sfVector2f pos = game->board->mouse_pos;
+    pos.x /= game->board->scale.x;
+    pos.y /= game->board->scale.y;
     for (int i = 0; i < radius; i++) {
         for (int j = 0; j < radius; j++) {
             (i * i + j * j <= radius * radius
             && i * i + j * j >= (radius - BRUSH_SIZE) * (radius - BRUSH_SIZE)
-            && pos.x + i < sfSprite_getTextureRect(\
+            && pos.x + i < sfSprite_getGlobalBounds(\
             game->board->sprite).width
             && pos.x - i >= 0 && pos.y - j >= 0
-            && pos.y + j < sfSprite_getTextureRect(\
+            && pos.y + j < sfSprite_getGlobalBounds(\
             game->board->sprite).height) ? ({
             sfImage_setPixel(IMAGE, pos.x + i, pos.y + j, color);
             sfImage_setPixel(IMAGE, pos.x - i, pos.y + j, color);
